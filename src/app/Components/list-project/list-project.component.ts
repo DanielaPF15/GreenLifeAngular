9013e7e1de53796c1a8ded603750a36762b183f6
lilianadiaz91 <bdiaz24@misena.edu.co>
2020-11-26 16:28:24 -0500

    se agregan cambios de alerts

--- src/app/Components/list-project/list-project.component.ts
+++ src/app/Components/list-project/list-project.component.ts
@@ -52,7 +52,11 @@ export class ListProjectComponent implements OnInit {
     if(this.createProjectForm.valid){
       this.projectService.createProject(this.createProjectForm.value).subscribe(
         (proyectcreated)=>{
-          alert('El Proyecto se creo correctamente')
+          swal({
+            title: "Hecho!",
+            text: "Proyecto creado correctamente!",
+            icon: "success",
+          });
           this.router.navigateByUrl('/', { skipLocationChange: true } ).then(
             () => {
               this.router.navigate(['/list-project'])
@@ -64,7 +68,11 @@ export class ListProjectComponent implements OnInit {
         }
       )
     } else{
-      alert('Todos los campos debes estar llenos')
+      swal({
+        title: "Error!",
+        text: "Todos los campos debes estar diligenciados",
+        icon: "error",
+      });
     }
   }
 
@@ -157,7 +165,11 @@ export class ListProjectComponent implements OnInit {
       )
 
     }else{
-      alert('Todos los campos debes estar llenos')
+      swal({
+        title: "Error!",
+        text: "Todos los campos deben estar diligenciados!",
+        icon: "error",
+      });
     }
 
   }
